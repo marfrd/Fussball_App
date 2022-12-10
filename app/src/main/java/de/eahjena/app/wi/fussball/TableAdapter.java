971e8037d9b985fd77ea74fcd8ba7f9e1f571bd2
Mario Friedrich <mario_f@gmx.net>
2022-12-10 19:50:53 +0100

    Four Columns

--- app/src/main/java/de/eahjena/app/wi/fussball/TableAdapter.java
+++ app/src/main/java/de/eahjena/app/wi/fussball/TableAdapter.java
@@ -1,5 +1,6 @@
 package de.eahjena.app.wi.fussball;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,26 +8,51 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class TableAdapter extends RecyclerView.Adapter<TableAdapter.ViewHolder> {
 
     private String[] localDataSet;
+
+    Context context;
+    List<TableTeam> tableTeamList;
 
     /**
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder).
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView tvPlace;
+        // private final ImageView ivLogo;
+        private final TextView tvTeam;
+        private final TextView tvGoals;
+        private final TextView tvPoints;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
 
-            textView = (TextView) view.findViewById(R.id.textView);
+            tvPlace = (TextView) view.findViewById(R.id.tv_place);
+            // ivLogo = (ImageView) view.findViewById(R.id.iv_logo);
+            tvTeam = (TextView) view.findViewById(R.id.tv_teams);
+            tvGoals = (TextView) view.findViewById(R.id.tv_goals);
+            tvPoints = (TextView) view.findViewById(R.id.tv_points);
         }
 
-        public TextView getTextView() {
-            return textView;
+        public TextView getTvPlace() {
+            return tvPlace;
+        }
+
+        public TextView getTvTeam() {
+            return tvTeam;
+        }
+
+        public TextView getTvGoals() {
+            return tvGoals;
+        }
+
+        public TextView getTvPoints() {
+            return tvPoints;
         }
     }
 
@@ -38,6 +64,8 @@ public class TableAdapter extends RecyclerView.Adapter<TableAdapter.ViewHolder> 
      */
     public TableAdapter(String[] dataSet) {
         localDataSet = dataSet;
+        // this.context = context;
+        // this.tableTeamList = tableTeamList;
     }
 
     // Create new views (invoked by the layout manager)
@@ -56,7 +84,10 @@ public class TableAdapter extends RecyclerView.Adapter<TableAdapter.ViewHolder> 
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText(localDataSet[position]);
+        viewHolder.getTvPlace().setText(localDataSet[position]);
+        viewHolder.getTvTeam().setText(localDataSet[position]);
+        viewHolder.getTvGoals().setText(localDataSet[position]);
+        viewHolder.getTvPoints().setText(localDataSet[position]);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
